649f02b367192757d9a116b2d0a62b49d2517118
renren <148751281+renrenbatabata@users.noreply.github.com>
2025-05-13 14:12:11 +0900

    選択画面作成

---
+++ frontend/lib/app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'screens/select_target/select_target_screen.dart';
+// import 'screens/take_photo/take_photo_screen.dart';
+// import 'screens/preview/preview_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'かながわく まち図鑑',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'NotoSansJP',
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      initialRoute: '/select_target', // 最初の画面
+      routes: {
+        '/select_target': (context) => const SelectTargetScreen(),
+        // '/take_photo': (context) => const TakePhotoScreen(),
+        // '/preview': (context) => const PreviewScreen(),
+      },
+    );
+  }
+}
