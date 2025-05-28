26e669213eabbddf838bb24e81622fb34c6dd7cf
renren <148751281+renrenbatabata@users.noreply.github.com>
2025-05-28 11:43:22 +0900

    フォント変更

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -3,9 +3,7 @@ import 'screens/select_target/select_target_screen.dart';
 import 'screens/start_page/start_page.dart';
 import 'screens/home_page/home_page.dart';
 import 'screens/zukan_page/zukan.dart';
-
-// import 'screens/take_photo/take_photo_screen.dart';
-// import 'screens/preview/preview_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,7 +14,8 @@ class MyApp extends StatelessWidget {
       title: 'かながわく まち図鑑',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        fontFamily: 'NotoSansJP',
+        // アプリ全体のフォントを設定
+        fontFamily: GoogleFonts.kosugiMaru().fontFamily,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
